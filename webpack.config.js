2a6003788b250ed8cf82145154d8c613cb7299be
veselyiJoker <isankazarez@gmail.com>
2022-08-22 09:50:38 +0300

    basic page layout

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+
+console.log('mode = ' + mode);
+
+module.exports = {
+    mode,
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext][query]',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: './src/index.html',
+            }
+        ),
+        new MiniCssExtractPlugin(
+            {
+                filename: '[name].[contenthash].css',
+            }
+        ),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        'postcss-preset-env',
+                                    ]
+                                ]
+                            }
+                        }
+                    },
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+}
+
